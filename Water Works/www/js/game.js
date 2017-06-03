2cd9876de88bc944b2edd124bf5183e4a60493e0
TheCodeFactory <calebhwin@gmail.com>
2017-06-03 16:43:47 -0700

    Update game.js

--- Water Works/www/js/game.js
+++ Water Works/www/js/game.js
@@ -2,14 +2,19 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
 }
 
-var x = 0;
+var x = -width/2;
 var width,height;
 var page = 1;
 function game() {
     background(234, 198, 122);
     fill(35, 50, 55);
-    ellipse(width/2, x, 100, 100);
-    x+=(height/2-x)/10;
+    stroke(152, 75, 67);
+    noFill();
+    strokeWeight(13);
+    line(width/4,height-x,width/4,height*3/4-x);
+    line(width*3/4,height-x,width*3/4,height*3/4-x);
+    line(width/2,height-x,width/2,height*3/4-x);
+    x+=(0-x)/10;
 }
 
 function draw() {
