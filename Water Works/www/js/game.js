e42f62eec01bc315d781c1e35ee1c6da79dc0512
TheCodeFactory <calebhwin@gmail.com>
2017-06-03 17:59:23 -0700

    Update game.js

--- Water Works/www/js/game.js
+++ Water Works/www/js/game.js
@@ -33,12 +33,12 @@ function game() {
     rect(0,height*27/32,width,height);
     beginShape();
     vertex(0, height*27/32);
-    bezierVertex(width*0/5, height*27/32+15*sin((frameCount-60)/5));
-    bezierVertex(width*1/5, height*27/32+15*sin((frameCount+0)/5));
-    bezierVertex(width*2/5, height*27/32+15*sin((frameCount+60)/5));
-    bezierVertex(width*3/5, height*27/32+15*sin((frameCount+120)/5));
-    bezierVertex(width*4/5, height*27/32+15*sin((frameCount+180)/5));
-    bezierVertex(width*5/5, height*27/32+15*sin((frameCount+240)/5));
+    curveVertex(width*0/5, height*27/32+15*sin((frameCount-60)/5));
+    curveVertex(width*1/5, height*27/32+15*sin((frameCount+0)/5));
+    curveVertex(width*2/5, height*27/32+15*sin((frameCount+60)/5));
+    curveVertex(width*3/5, height*27/32+15*sin((frameCount+120)/5));
+    curveVertex(width*4/5, height*27/32+15*sin((frameCount+180)/5));
+    curveVertex(width*5/5, height*27/32+15*sin((frameCount+240)/5));
     vertex(width, height*27/32);
     endShape();
 }
