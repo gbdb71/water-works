e65dd901ce829a4ceea26cb9db5aab34a7889c2f
TheCodeFactory <calebhwin@gmail.com>
2017-06-03 17:54:44 -0700

    Update game.js

--- Water Works/www/js/game.js
+++ Water Works/www/js/game.js
@@ -33,12 +33,12 @@ function game() {
     rect(0,height*27/32,width,height);
     beginShape();
     vertex(0, height*27/32);
-    quadraticVertex(0, height*27/32+15*sin((frameCount-60)/15));
-    quadraticVertex(width*1/5, height*27/32+15*sin((frameCount+0)/15));
-    quadraticVertex(width*2/5, height*27/32+15*sin((frameCount+60)/15));
-    quadraticVertex(width*3/5, height*27/32+15*sin((frameCount+120)/15));
-    quadraticVertex(width*4/5, height*27/32+15*sin((frameCount+180)/15));
-    quadraticVertex(width, height*27/32+15*sin((frameCount+240)/15));
+    quadraticVertex(width*0/5, height*27/32+15*sin((frameCount-60)/5));
+    quadraticVertex(width*1/5, height*27/32+15*sin((frameCount+0)/5));
+    quadraticVertex(width*2/5, height*27/32+15*sin((frameCount+60)/5));
+    quadraticVertex(width*3/5, height*27/32+15*sin((frameCount+120)/5));
+    quadraticVertex(width*4/5, height*27/32+15*sin((frameCount+180)/5));
+    quadraticVertex(width*5/5, height*27/32+15*sin((frameCount+240)/5));
     vertex(width, height*27/32);
     endShape();
 }
