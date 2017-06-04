fbedf4263b875d3258e5917fd5376430f401d95f
TheCodeFactory <calebhwin@gmail.com>
2017-06-03 17:13:03 -0700

    Update game.js

--- Water Works/www/js/game.js
+++ Water Works/www/js/game.js
@@ -14,7 +14,7 @@ function game() {
     push();
     translate(0,x);
     line(width*3/16,height*3/4,width*13/16,height*3/4);
-    for(var i = 1; i < 3; i++) {
+    for(var i = 1; i < 4; i++) {
         line(width*i/4,height*3/2,width*i/4,height*3/4);
     }
     pop();
@@ -25,7 +25,7 @@ function game() {
 function draw() {
     width = windowWidth;
     height = windowHeight;
-    x=-height/2;
+    x=-0;
     switch(page) {
         case 1:
             game();
