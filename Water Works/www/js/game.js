5767b8eab1e98a459ba2a1dde8f2846b6e6fa9b3
TheCodeFactory <calebhwin@gmail.com>
2017-06-03 17:02:50 -0700

    Update game.js

--- Water Works/www/js/game.js
+++ Water Works/www/js/game.js
@@ -7,14 +7,16 @@ var width,height;
 var page = 1;
 function game() {
     background(234, 198, 122);
-    fill(35, 50, 55);
-    stroke(152, 75, 67);
+    fill(247, 245, 230);
+    stroke(255, 106, 92);
     noFill();
     strokeWeight(13);
     line(width/4,height-x,width/4,height*3/4-x);
     line(width*3/4,height-x,width*3/4,height*3/4-x);
     line(width/2,height-x,width/2,height*3/4-x);
+    //line(width/2,height-x,width/2,height*3/4-x);
     x+=(0-x)/10;
+    alert(5);
 }
 
 function draw() {
